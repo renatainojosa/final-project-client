1dc180418ea5a482b6b2a55c1a3aef2cbd2a8b4d
Renata Inojosa <inojosa.renata@gmail.com>
2022-11-15 12:01:49 -0300

    criado cadastro de usuario

--- src/pages/SignUpUser/SignUpUser.js
+++ src/pages/SignUpUser/SignUpUser.js
@@ -1,10 +1,31 @@
-import './SignUpUser.css';
-import { useState } from 'react';
+import "./SignUpUser.css";
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+import UserForm from "../../components/Forms/UserForm/UserForm";
+import api from "../../api/project.api";
 
 const SignUpUser = () => {
+    const [loading, setLoading] = useState(false)
+    const navigate = useNavigate();
+    
+    const onSubmit = async ({username, email, password, contact, profileImgUrl}) => {
+        setLoading(true)
+        try {
+            await api.signup({username, email, password, contact, profileImgUrl});
+            navigate('/login')
+        } catch (error) {
+            throw error.response.data || error.message || error;
+        } finally {
+            setLoading(false);
+        }
+    }
+
   return (
-    <div>SignUpUser</div>
-  )
-}
+  <div className="signup-container">
+    <h2 className="title">Signup</h2>
+    <UserForm onSubmit={onSubmit} submitText='Register' loading={loading} />
+  </div> 
+  );
+};
 
 export default SignUpUser;
