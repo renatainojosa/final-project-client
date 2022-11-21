e1605214915eb41271ae4eeeb349cd9268d5aa9b
Walison Abram <abramwhas@gmail.com>
2022-11-21 17:36:51 -0300

    add card my pets e pets

--- src/pages/MyPets/MyPets.js
+++ src/pages/MyPets/MyPets.js
@@ -20,19 +20,32 @@ const MyPets = () => {
         })
     }, [])
   return (
-    <div>
+    <>
       <h2>My Pets</h2>
+    <div className='my-pets-container'>
       {myPets.map((pet) => {
         return (
-          <div key={pet._id}>
-            <h4>{pet.name}</h4>
-            <Link to={`/my-profile/my-pets/${pet._id}/edit`}><button>Edit</button></Link>
-            <button>Remove</button>
-            {isLoading && 'Loading...'}
+          // <div key={pet._id}>
+          //   <h4>{pet.name}</h4>
+          //   <Link to={`/my-profile/my-pets/${pet._id}/edit`}><button>Edit</button></Link>
+          //   <button>Remove</button>
+          //   {isLoading && 'Loading...'}
+          // </div>
+          
+            <div className="card" style={{width: '18rem'}} key={pet._id}>
+              <img className='card-img-top' src={pet.profileImgUrl} alt='img pet' />
+              <div>
+                <h5 className="card-title">{pet.name}</h5>
+                <p className="card-text">{pet.description}</p>
+                <Link to={`/my-profile/my-pets/${pet._id}/edit`} className="btn btn-primary">Edit Pet</Link>
+                <Link to={`/my-profile/my-pets/${pet._id}/edit`} className="btn btn-primary">Remove Pet</Link>
+                {isLoading && 'Loading...'}
+              </div>
           </div>
         ) 
       })}
     </div>
+    </>
   )
 };
 
