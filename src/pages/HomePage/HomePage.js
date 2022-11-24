daf2a7669e40da09c70a359eb72b5e7f7aaa4207
Renata Inojosa <inojosa.renata@gmail.com>
2022-11-23 21:32:08 -0300

    css homepage

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,5 +1,6 @@
 import "./HomePage.css";
 import { Link } from "react-router-dom"
+import iconHeart from "../../images/icon-heart.png"
 
 const HomePage = () => {
   
@@ -8,20 +9,15 @@ const HomePage = () => {
     <div className="home-container">
       <div className="home-text">
         <h1>Welcome!</h1>
-        <h5>
+        <h4>
           Here you will met the cutiest pets who needs love and a home! 
-        </h5>
-      <div>
+        </h4>
         <Link to='/instructions'>
           <button className="btn btn-warning btn-lg">
             How do I adopt a pet?
           </button>
         </Link>
       </div>
-      </div>
-      {/* <div className="pet-img">
-        <img className="img-pets" src={imgPets} alt="img-pets" />
-      </div> */}
     </div>
   );
 };
