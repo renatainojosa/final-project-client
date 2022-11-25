143acb1f3464c82b14ded87ff4c83557a77ed5b5
Renata Inojosa <inojosa.renata@gmail.com>
2022-11-25 19:33:45 -0300

    pagina ongs

--- src/pages/Ongs/Ongs.js
+++ src/pages/Ongs/Ongs.js
@@ -1,8 +1,8 @@
-import './Ongs.css'
+import "./Ongs.css";
 import api from "../../api/project.api";
-import { useState, useEffect } from 'react';
-import CardOng from '../../components/CardOng/CardOng';
-import { firstLetter } from '../../utils/other.utils';
+import { useState, useEffect } from "react";
+import CardOng from "../../components/CardOng/CardOng";
+import { firstLetter } from "../../utils/other.utils";
 
 const Ongs = () => {
   const [ongs, setOngs] = useState([]);
@@ -19,19 +19,25 @@ const Ongs = () => {
   }, []);
 
   return (
-    <div className='ongs-container'>
-      {ongs.map((ong) => {
-        return (
-          <CardOng 
-            keyId={ong._id}
-            ongImg={ong.profileImgUrl}
-            ongName={firstLetter(ong.name)}
-            route={`/ongs/${ong._id}/about`}
-            text='About this ONG' />
-        )
-      })}
+    <div className="ongs-container">
+      <div className="title-ongs">
+        <h2>ONGs</h2>
+      </div>
+      <div className="ongs-list">
+        {ongs.map((ong) => {
+          return (
+            <CardOng
+              keyId={ong._id}
+              ongImg={ong.profileImgUrl}
+              ongName={firstLetter(ong.name)}
+              route={`/ongs/${ong._id}/about`}
+              text="About this ONG"
+            />
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Ongs;
