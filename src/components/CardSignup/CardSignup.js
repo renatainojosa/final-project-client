3a3de1f3221c9adf607b8bfe2a1ed944bf4ecfb9
Walison Abram <abramwhas@gmail.com>
2022-11-25 00:08:51 -0300

    signup form pronto

--- src/components/CardSignup/CardSignup.js
+++ src/components/CardSignup/CardSignup.js
@@ -4,10 +4,9 @@ import { Link } from "react-router-dom";
 const CardSignup = ({ route, image, type }) => {
   return (
     <div className="card-signup text-center" style={{ width: "18rem" }}>
-    <img src={image} className="card-img-top" alt="card-img-top" />
+    <img src={image} style={{width: '5rem'}} className="card-img-top" alt="card-img-top" />
       <div className="card-body">
-        <h5 className="card-title">Special title treatment</h5>
-        <Link to={route} className="btn btn-primary">
+        <Link to={route} className="btn btn-new-pet">
           {type}
         </Link>
       </div>
--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,5 +1,6 @@
 import "./SignUp.css";
 import CardSignup from "../../components/CardSignup/CardSignup";
+import imageProfile from '../../images/avatar-profile.png'
 
 const SignUp = () => {
   return (
@@ -8,8 +9,8 @@ const SignUp = () => {
         <h2>Who you are?</h2>
       </div>
         <div className="cards">
-          <CardSignup type="I am a Person" route="/signup/user" image="image" />
-          <CardSignup type="I am a ONG" route="/signup/ong" image="image" />
+          <CardSignup type="I am a Person" route="/signup/user" image={imageProfile} />
+          <CardSignup type="I am a ONG" route="/signup/ong" image={imageProfile} />
         </div>
       </div>
   );
