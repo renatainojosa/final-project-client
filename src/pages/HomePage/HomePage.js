042275c4bb199aa547e401454087a1d532eff94f
Walison Abram <abramwhas@gmail.com>
2022-11-26 09:26:27 -0300

    ajuste no home page

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -9,8 +9,8 @@ const HomePage = () => {
         <h2>Welcome!</h2>
         <h3>Who is Joseph?</h3>
         <h4>
-          Joseph is a orange cute and lovely cat who was adopted when he is a baby. Today, he's healthy and has a home where is a place full of love. 
-          So, him history is our inspiration to motivate other people to do the same thing :D
+          Joseph is an orange cute and lovely cat who was adopted when he was a baby. Today, he's healthy and has a home where is a place full of love. 
+          So, his history is our inspiration to motivate other people to do the same thing :D
         </h4>
       </div>
     </div>
