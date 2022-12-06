0927eee88898d12f3aec38520b44fc3f5fe9de07
Renata Inojosa <inojosa.renata@gmail.com>
2022-12-05 21:08:54 -0300

    componente alert feito

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -3,11 +3,13 @@ import LoginForm from "../../components/Forms/LoginForm/LoginForm";
 import { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../../api/project.api";
+import Alert from "../../components/Alert/Alert";
 
 import { AuthContext } from "../../context/auth.context";
 
 const Login = () => {
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
   const navigate = useNavigate();
 
   const { authenticateUser } = useContext(AuthContext);
@@ -17,9 +19,10 @@ const Login = () => {
     try {
       await api.login({ email, password });
       authenticateUser();
+      console.log('deu erro')
       navigate("/");
     } catch (error) {
-      throw error.response.data || error.message || error;
+      setError(error);
     } finally {
       setLoading(false);
     }
@@ -38,10 +41,13 @@ const Login = () => {
   };
 
   return (
+    <>
+      {error && <Alert message='E-mail or password is invalid!' />}
     <div className="login-container login-page login-mobile">
       <LoginForm type='Login' onSubmit={onSubmit} submitText="Submit" loading={loading} />
       <LoginForm type='Login ONG' onSubmit={onSubmitOng} submitText="Submit" loading={loading} />
     </div>
+    </>
   );
 };
 
